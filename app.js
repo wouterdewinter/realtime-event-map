ae2ceccaa93cc90b178c67e79dfdebd682cf4ffd
Wouter de Winter <wouter.de.winter@clansman.nl>
2016-10-28 18:57:40 +0200

    more tryout

--- app.js
+++ app.js
@@ -3,10 +3,6 @@ var io = require('socket.io')(app);
 var fs = require('fs');
 var maxmind = require('maxmind');
 
-// maxmind.open(__dirname + '/data/GeoLite2-City.mmdb', (err, cityLookup) => {
-//   var city = cityLookup.get('66.6.44.4');
-//   console.log(city.location.latitude, city.location.longitude);
-// });
 var cityLookup = maxmind.openSync(__dirname + '/data/GeoLite2-City.mmdb');
 
 app.listen(8080);
@@ -27,18 +23,18 @@ function handler (req, res) {
 io.on('connection', function (socket) {
   socket.on('event', function (data) {
     var city = cityLookup.get(data.ip);
-    if (city !== null) {
+    if (city !== null && city.location !== undefined) {
       data = {
         x: city.location.longitude,
         y: city.location.latitude
       }
       socket.broadcast.emit('hit', data);
       console.log(data);
+    } else {
+      console.log("lookup failed for " + data.ip)
     }
-  });
-});
 
-io.on('connection', function (socket) {
-  var ip = socket.request.connection.remoteAddress;
-  console.log('IP is '+ip);
+    var ip = socket.request.connection.remoteAddress;
+    console.log('IP is '+ip);
+  });
 });
